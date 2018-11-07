bd8de7e1af86d15395af85e79820fb6c3de746fd
alforsyth <aforsyth22@gwu.edu>
2018-11-06 20:20:37 -0500

    Callie Sreya Sydney working on combining stack and working with the sensor

--- RGBtoHSVLineMazeFollow.java
+++ RGBtoHSVLineMazeFollow.java
@@ -1,4 +1,4 @@
-
+import java.util.Stack;
 	import lejos.hardware.Button;
 	import lejos.robotics.Color;
 	import lejos.utility.Delay;
@@ -81,12 +81,26 @@ import lejos.hardware.motor.*;
 		    
 		    Button.waitForAnyPress(); 
 		    
-			boolean touched = false;
 		    
-			double [] hsv = new double[3];			
+			double [] hsv = new double[3];
 			
+			Stack<Integer> stack = new Stack<Integer>();
+            //created a new stack called stack
+            //used later at intersections
+            
+            boolean touched = false; 
+            //check this later
+            //initialize the boolean touched variable to either true or false
+            //does it matter which?
+            
+            int x;
+            //initializing variable x that will store the top of the stack
+			
+            boolean mazeSolved = false;
+			
+			//if (mazeSolved == false) {	
 				
-				while (!Button.ESCAPE.isDown()) {
+            while (!Button.ESCAPE.isDown()) {
 					//System.out.println ("starting while loop");
 					rgb = colorSensor.getColor();
 					hsv= RGBtoHSV(rgb);
@@ -108,103 +122,177 @@ import lejos.hardware.motor.*;
 					//make the middle range smaller?
 				
 					//when the robot is on the middle it will go forward
-					 if ((hsv[0]>30)&&(hsv[0]<34)||(hsv[0]>=47)&&(hsv[0]<61)) { 
+					 if ((hsv[0]>14)&&(hsv[0]<20)||(hsv[0]>25)&&(hsv[0]<30)||(hsv[0]>40)&&(hsv[0]<61)) { 
 						 System.out.println("middle");
 						 pilot.travel(3);
 						 System.out.println (hsv[0]);
 							
 					 }
 					 //when the robot is on black it will rotate and fix its self back on the middle
-					 else if ((hsv[0]>=90)&&(hsv[0]<200)) {//||(hsv[0]==0.0)||(hsv[0]>50&&hsv[0]<125)) {
+					 else if ((hsv[0]>=90)&&(hsv[0]<106)||(hsv[0]>118)&&(hsv[0]<241)) {//||(hsv[0]==0.0)||(hsv[0]>50&&hsv[0]<125)) {
 						 System.out.println("black line");
 							pilot.rotate(5);
 							System.out.println (hsv[0]);
 							
 					}
 					 //when the robot is in the wood, it will fix itself back on the line
-					 else if ((hsv[0]>=34)&&(hsv[0]<47)) {
+					 else if ((hsv[0]>=20)&&(hsv[0]<25)||(hsv[0]>=30)&&(hsv[0]<=40)) {
 					 
 						 System.out.println ("floor");						 
 						 pilot.rotate(-6); 						
 						 System.out.println (hsv[0]);
 						 
 					 }
-
 				
 					 
+					//SILVER fix the colors
+	                   else if ((hsv[0]>81)&&(hsv[0]<90)||(hsv[0]>106)&&(hsv[0]<=118)) { 
+	                	   
+	                	   System.out.println("Silver detected turn around");
+	                	   mazeSolved = true;
+	                	   
+	                	   pilot.travel(-.2);
+	                	   pilot.rotate(-160);
+	                	   pilot.travel(7);
+	                	   pilot.rotate(60);
+	                	   
+	                	   //continue line following
+	                	   
+	                	 //when the robot is on the middle it will go forward
+	  					 if ((hsv[0]>14)&&(hsv[0]<20)||(hsv[0]>25)&&(hsv[0]<30)||(hsv[0]>40)&&(hsv[0]<61)) { 
+	  						 System.out.println("middle");
+	  						 pilot.travel(3);
+	  						 System.out.println (hsv[0]);
+	  							
+	  					 }
+	  					 //when the robot is on black it will rotate and fix its self back on the middle
+	  					 else if ((hsv[0]>=90)&&(hsv[0]<106)||(hsv[0]>118)&&(hsv[0]<241)) {//||(hsv[0]==0.0)||(hsv[0]>50&&hsv[0]<125)) {
+	  						 System.out.println("black line");
+	  							pilot.rotate(5);
+	  							System.out.println (hsv[0]);
+	  							
+	  					}
+	  					 //when the robot is in the wood, it will fix itself back on the line
+	  					 else if ((hsv[0]>=20)&&(hsv[0]<25)||(hsv[0]>=30)&&(hsv[0]<=40)) {
+	  					 
+	  						 System.out.println ("floor");						 
+	  						 pilot.rotate(-6); 						
+	  						 System.out.println (hsv[0]);
+	  						 
+	  					 }
+	  					 else if ((hsv[0]>=0)&&(hsv[0]<10)) { 
+	                	   //red is detected
+	  						 	pilot.travel(4);
+	                	   		stack.pop();
+	  					}
+							
+	               } 	   
+	                	   
+	                	   //if the color detected is equal to the color of the silver box (ending indicator)
+	                     //may need to use ambient light mode
+	                     //when color reaches the foil (silver)
+	                        //then indicate that the robot should executing what's stacked
+	                     
+	                     //go through the stack but SWITCHED
+	                     //add another boolean variable (mazeSolved) -> indicated maze has now been solved
+	                     
+	                     //use line following
+	                     //structure:
+	                     //if (mazeSolved == true)
+	                        //switch stack & go back through the maze
+	                           //create case statements
+	                              //look at powerpoint
+	                              //case 0 = ...
+	                              //1 should be turned to 2 and vice versa
+	                        //1 and 2 will mean the opposite of what they meant when we were going through the maze from start to finish
+	                     //else if (mazeSolved = false)
+	                        //go through intersection code (red color)
+	                     
+	                     		
+					 
 					 //need to implement when the robot sees a new color (red) then it with turn or do as it is called to to
-					 else if ((hsv[0]>5)&&(hsv[0]<10)) {
+					 else if ((hsv[0]>=0)&&(hsv[0]<10)) {
 						 
 						 System.out.println("red intersection");
-						 //ir code activated
-	      					distanceir.fetchSample(sampleir, 0);
-	      					if (sampleir[0]<=8) {
-	                              //do not turn
-	                              //put another statements saying if it's NOT activated
-	                              //if its  not activated then turn right over intersections
-	      						  //travels back on to the line when it travels 5
-	                            pilot.travel(5);
-	      					}  
-	      					//attach stacks to the movements
-	      					//straight = 0;
-	      					//inside conditional
-	      					
-	      					// rotates to the right if the right is clear
-		                     else {
-		                            pilot.travel(3);
-		                            pilot.rotate(-80);
-		                            pilot.travel(3);
-		                     }	
-	      					//attach stacks to the movements
-	      					//turn right = 2
-	      					//inside conditional
-	      					if (touched==true) {
-	      						//all combinations go here
-	      						touched=false;
-	      					}
-					 }
-					 
-					 else {
-						 System.out.println("hey");
-						 pilot.travel(.2);
-					 }
-					 //when the robot sees silver, then it should recognize that it is at the end of the maze and call to the stacking method to retrace its steps
-					 /*else if ((hvs[0]>110)&&(hsv[0]<135)){ SILVER
-					  * 83-95
-					 }*/
-					 
-					 
-					 //touch sensor code
-					 /*distance.fetchSample(sample, 0);
-						if (sample[0]== 1) {
-							
-							pilot.travel(-10);
-							pilot.rotate(-90);
-							pilot.travel(20);
-							pilot.rotate(90);
-							pilot.forward();
+						 if (touched = false) {
+		                     //ir code activated
+		      					distanceir.fetchSample(sampleir, 0);
+		      					if (sampleir[0]<=20) {
+	                                 //measured distance between edge of intersection square and wall => 20 cm
+		                              //do not turn
+		                              //put another statements saying if it's NOT activated
+		                              //if its  not activated then turn right over intersections
+		      						  //travels back on to the line when it travels 5
+		                            pilot.travel(5);
+		      					}  
+		      					//attach stacks to the movements
+		      					//straight = 0;
+		      					//inside conditional
+		      					
+		      					// rotates to the right if the right is clear
+			                     else {
+			                    	 	pilot.travel(3);
+			                            pilot.rotate(-80);
+			                            pilot.travel(3);
+			                     }	
+		      					//attach stacks to the movements
+		      					//turn right = 2
+		      					//inside conditional
+	                        
+	                        //what the #'s mean:
+	                        //0 = go straight, 1 = turn left, 2 = turn right
+		      					//if (touched = false) { DELETE
+	                           //check IR ()
+	                           if (sampleir[0]<=20) { // IR < 20
+	                              pilot.forward();
+	                              stack.push(0);
+	                              //add 0 to the stack
+	                           }
+						 }
+						 else if (touched==true) {
+	                           touched = false;
+	                              //touched is now assigned with false
+	                           x = stack.peek();
+	                         
+	                              //peek = tells you the top element in the stack
+	                              //x is initialized outside out of the loop    
+	                           
+	                           //pop the top of the stack
+	                           stack.pop();
+	                           
+	                           if (x == 0) {
+	                              //turn right
+	                              pilot.rotate(-90);
+	                              //store as left (1) on stack
+	                              stack.push(1);
+	                           }
+	                           else if (x == 2) {
+	                              //check right
+	                              if (sampleir[0]<=20) { // IR < 20
+	                                 pilot.forward();
+	                                 stack.push(0);
+	                                 //add 0 to the stack
+	                              }
+	                              else {
+	                                 //turn right
+	                                 pilot.rotate(-90);
+	                                 //store as straight (0) on stack
+	                                 stack.push(0);
+	                              }
+	                           }
+	                        }
+		      				   //all combinations are above (only 3 are relevant)
+		      			 }
+			                   
+						else {
+							System.out.println (hsv[0] + "correction");
+							pilot.travel(.2);
 						}
-						
-					//ir code
-					distance.fetchSample(sample, 0);
-						if (sample2[0]<=18) {
-							
-							pilot.travel(-10);
-							pilot.rotate(-90);
-							pilot.travel(20);
-							pilot.rotate(90);
-							pilot.forward();
-						}*/
+					 
 					 
 			}
 				
-				//add hsv for silver end of the maze, and add red for intersections
-				/*if the robot sees red and cannot turn to the right (ir sensor) then go straight
-				 * 	then if it cant go forward, turn around and when you get back to the red line then turn left
-				 * if the robot sees red and can turn right, then turn right
-				 * if the robot hits something in front of itself, then have the robot turn around to the right				
-				 * 
-				 */
+				
 				
 			
 			
