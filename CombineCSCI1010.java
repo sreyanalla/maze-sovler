2b1ae1b5ff9dbdb8c60063f609f9066ff96e275a
alforsyth <aforsyth22@gwu.edu>
2018-11-06 20:23:02 -0500

    file to test the IR colors by placing the robot down and printing the colors on the screen

---
+++ CombineCSCI1010.java
@@ -0,0 +1,101 @@
+import java.util.Stack;
+	import lejos.hardware.Button;
+	import lejos.robotics.Color;
+	import lejos.utility.Delay;
+	import lejos.hardware.port.*;
+import lejos.hardware.sensor.EV3IRSensor;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.SensorMode;
+import lejos.hardware.motor.*;
+	import lejos.robotics.SampleProvider;
+	import  lejos.robotics.chassis.Chassis; 
+	import  lejos.robotics.chassis.Wheel; 
+	import  lejos.robotics.chassis.WheeledChassis;
+	import lejos.robotics.navigation.MovePilot;
+
+	public class CombineCSCI1010 {
+		
+		public static double[] RGBtoHSV(Color colors){
+			double[] hsv = new double[3];
+			// read colors
+			int r = colors.getRed();
+			int b = colors.getBlue();
+			int g = colors.getGreen();
+			
+			double min = Math.min(r, Math.min(b,g));
+			double max = Math.max(r, Math.max(b, g));
+			double delta = max - min;
+			hsv[2] = max/255; //set v to max as a percentage
+			if (max != 0){
+				hsv[1] = delta/max;
+			}
+			else{ //r = b = g =0 
+				hsv[1] = 0; //s = 0;		// s = 0, v is undefined
+				hsv[0] = -1; //h = -1;
+				return hsv;
+			}
+			
+			if (r == max){
+				hsv[0] = (g-b)/delta; //h 
+			}
+			else{
+				if (g == max)
+					hsv[0] = 2 + (b - r)/delta; //h
+				else
+					hsv[0] = 4 + (r - g)/delta; //h
+			}
+			
+			hsv[0] *=60;	//degrees
+			if (hsv[0] < 0) {
+				hsv[0] +=360;
+			}
+			return hsv;
+		}
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			ColorSensor colorSensor = new ColorSensor(SensorPort.S3);
+			colorSensor.setRGBMode();
+			Color rgb; 
+			
+			EV3LargeRegulatedMotor motora = new EV3LargeRegulatedMotor (MotorPort.A);
+			EV3LargeRegulatedMotor motorb = new EV3LargeRegulatedMotor (MotorPort.C);
+			EV3TouchSensor touchsensor = new EV3TouchSensor (SensorPort.S2);
+			EV3IRSensor irSensor = new EV3IRSensor (SensorPort.S1);
+			
+			Wheel wheel1 = WheeledChassis.modelWheel(motora , 2.5).offset(-5.0);
+		    Wheel wheel2 = WheeledChassis.modelWheel(motorb , 2.5).offset(5.0);
+		    
+		    Chassis chassis = new WheeledChassis(new  Wheel[] { wheel1, wheel2 }, WheeledChassis.TYPE_DIFFERENTIAL);
+		    
+		  //distance fetch sample code
+		    SensorMode distanceir = irSensor.getDistanceMode();
+		    float[] sampleir = new float[1];
+		    
+			//touch sensor array code
+		    SampleProvider distancetouch = touchsensor.getTouchMode();				
+			float[] sampletouch = new float[1];
+		    
+			//is pressed method? do we need to make float values and fetching samples?
+		    MovePilot pilot = new MovePilot(chassis);
+		    
+		    Button.waitForAnyPress(); 
+		    
+		    
+			double [] hsv = new double[3];
+
+				
+            while (!Button.ESCAPE.isDown()) {
+					//System.out.println ("starting while loop");
+					rgb = colorSensor.getColor();
+					hsv= RGBtoHSV(rgb);
+					
+					
+					Delay.msDelay(200);
+					
+					System.out.println(hsv[0]);
+					
+            }
+            colorSensor.close();
+		}
+	}
